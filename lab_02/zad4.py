e58b7bb17a364e737e2fc25ea891ef9464a6337c
AdrianAlbrecht <155376@student.uwm.edu.pl>
2023-03-02 15:52:27 +0100

    datetime.now()

--- lab_02/zad4.py
+++ lab_02/zad4.py
@@ -8,6 +8,6 @@ print(f'{3.141592653589793:f}')
 #3 Dystans pomiędzy znakiem aliczbą dziesiętną
 print(f'{23:=+7d}')
 #4 Datetime
-print(f'{datetime(2001, 2, 3, 4, 5):%Y-%m-%d %H:%M}')
+print(f'{datetime.now():%Y-%m-%d %H:%M}')
 #5 Wyrównanie i długość stringa
 print(f'{"tekst":{"^"}{20}}')
