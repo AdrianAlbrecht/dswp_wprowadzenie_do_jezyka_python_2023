14be94d7023e253077930fb09fc887579817c0ce
AdrianAlbrecht <155376@student.uwm.edu.pl>
2023-03-02 15:54:03 +0100

    minor fix :)

--- lab_02/zad4.py
+++ lab_02/zad4.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 
-#1 Cięcie stringa
-print(f'{"Xylophone":.5}')
+#1 Cięcie stringa z określoną długością
+print(f'{"Xylophone":10.5}!')
 #2 Formatowanie floata
 print(f'{3.141592653589793:f}')
 #3 Dystans pomiędzy znakiem aliczbą dziesiętną
